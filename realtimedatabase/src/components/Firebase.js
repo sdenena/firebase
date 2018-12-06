de0deaaa5f31cb16b4f5ae9671c2d6f43f05374f
songden <songden007@gmail.com>
2018-12-06 16:58:59 +0700

    fix Firebase

--- realtimedatabase/src/components/Firebase.js
+++ realtimedatabase/src/components/Firebase.js
@@ -1,13 +1,11 @@
-import React from 'react'
 import firebase from 'firebase'
-
-
-var config = {
+const firebaseConfig = {
     apiKey: "AIzaSyChxFDavmxp9x2OkSG2WMjJXSbfnoYIvUs",
     authDomain: "realtimedb-e4733.firebaseapp.com",
     databaseURL: "https://realtimedb-e4733.firebaseio.com",
     projectId: "realtimedb-e4733",
     storageBucket: "realtimedb-e4733.appspot.com",
     messagingSenderId: "381006956520"
-  };
-  export const FireBase=firebase.initializeApp(config);
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default  firebaseApp;
